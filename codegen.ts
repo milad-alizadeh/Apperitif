6c829f1f0606b7f425866f58374ca3b50f66feef
Milad Alizadeh <milaadd@gmail.com>
2023-09-29 01:02:17 +0100

    add gql generations

--- codegen.ts
+++ codegen.ts
@@ -1,9 +1,11 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
-import 'dotenv/config'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: `.env.local` })
 
 const config: CodegenConfig = {
   schema: `${process.env.EXPO_PUBLIC_SUPABASE_URL}/graphql/v1`,
-  documents: ['/**/!(*.d).{ts,tsx}'],
+  documents: ['./**/!(*.d).{ts,tsx}'],
   generates: {
     './__generated__/': {
       preset: 'client',
