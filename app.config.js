087f822175e676fa431046c524289b1e7c4195fd
Milad Alizadeh <milaadd@gmail.com>
2023-10-03 23:53:51 +0100

    update eas config

---
+++ app.config.js
@@ -0,0 +1,48 @@
+const IS_DEV = process.env.APP_VARIANT === 'development'
+
+export default {
+  expo: {
+    name: IS_DEV ? 'Apperitif Dev' : 'Apperitif',
+    slug: 'apperitif',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/app-icon-all.png',
+    scheme: 'myapp',
+    userInterfaceStyle: 'automatic',
+    splash: {
+      image: './assets/images/splash-logo-all.png',
+      resizeMode: 'contain',
+      backgroundColor: '#FFFFFF',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      usesAppleSignIn: true,
+      supportsTablet: true,
+      bundleIdentifier: IS_DEV ? 'ai.bubblewrap.apperitif.dev' : 'ai.bubblewrap.apperitif',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/images/app-icon-android-adaptive-foreground.png',
+        backgroundImage: './assets/images/app-icon-android-adaptive-background.png',
+      },
+      package: IS_DEV ? 'ai.bubblewrap.apperitif.dev' : 'ai.bubblewrap.apperitif',
+    },
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/app-icon-web-favicon.png',
+    },
+    plugins: ['expo-router', 'expo-apple-authentication'],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: '0fe189be-8677-4ba8-b1e9-c3b3e63b388f',
+      },
+    },
+  },
+}
