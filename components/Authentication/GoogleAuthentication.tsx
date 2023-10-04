50b006d395e7b1629f8387e17a8d25595b244aa1
Milad Alizadeh <milaadd@gmail.com>
2023-10-04 04:01:17 +0100

    add google services info

---
+++ components/Authentication/GoogleAuthentication.tsx
@@ -0,0 +1,37 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin'
+import { useState } from 'react'
+
+export const GoogleAuthentication = function GoogleAuthentication({}) {
+  const [userInfo, setUserInfo] = useState(null)
+  // Somewhere in your code
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      setUserInfo(userInfo)
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  }
+
+  return (
+    <GoogleSigninButton
+      size={GoogleSigninButton.Size.Wide}
+      color={GoogleSigninButton.Color.Dark}
+      onPress={this._signIn}
+      disabled={this.state.isSigninInProgress}
+    />
+  )
+}
