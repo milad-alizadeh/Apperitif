5fb75260ef8d525bca2bf27fde21a97caf059917
Milad <milaadd@gmail.com>
2023-10-14 03:26:16 +0100

    update app.json

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-const APP_VARIANT = ''
+const APP_VARIANT = process.env.APP_VARIANT
 const APP_NAME = `Apperitif${APP_VARIANT ? `-${APP_VARIANT}` : ''}`
 const BUNDLE_ID = `ai.bubblewrap.apperitif${APP_VARIANT ? `.${APP_VARIANT}` : ''}`
 
