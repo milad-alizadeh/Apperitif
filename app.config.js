40e1a79b23c3338f9f64aadc98e0d675fde090b6
Milad <milaadd@gmail.com>
2023-10-17 16:37:53 +0100

    update to 1.1.0

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default () => {
   return {
     name: APP_NAME,
     slug: 'apperitif',
-    version: '1.0.0',
+    version: '1.1.0',
     orientation: 'portrait',
     icon: './assets/images/app-icon-all.png',
     scheme: BUNDLE_ID,
