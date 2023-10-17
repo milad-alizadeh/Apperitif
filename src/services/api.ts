9d639650af1e0ed4697bb8e228cd386697dad834
Milad <milaadd@gmail.com>
2023-10-17 17:28:24 +0100

    remove apollo cache

--- src/services/api.ts
+++ src/services/api.ts
@@ -71,14 +71,13 @@ class Api {
 
     this.setDefaults(cache)
 
-    if (process.env.NODE_ENV !== 'development') {
-      console.log('persisting cache')
-      await persistCache({
-        cache,
-        storage: new AsyncStorageWrapper(AsyncStorage),
-      })
-    }
-
+    // if (process.env.NODE_ENV !== 'development') {
+    //   console.log('persisting cache')
+    //   await persistCache({
+    //     cache,
+    //     storage: new AsyncStorageWrapper(AsyncStorage),
+    //   })
+    // }
 
     this.apolloClient = new ApolloClient({
       link: this.authLink.concat(this.httpLink), // Chain it with the httpLink
