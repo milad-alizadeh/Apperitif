f513e999ab547501928c33412bb8dc36762d5553
Milad <milaadd@gmail.com>
2023-11-10 15:03:53 +0000

    update button test file

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -1,6 +1,11 @@
 import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
+import { useHaptic } from '~/hooks/useHaptics'
 import { Button, ButtonProps } from './index'
+
+jest.mock('~/hooks/useHaptic', () => ({
+  useHaptic: () => jest.fn(),
+}))
 
 describe('Button', () => {
   it('renders correctly', () => {
@@ -14,8 +19,31 @@ describe('Button', () => {
   })
 
   it('renders large button when large is true', () => {
-    const { getByText } = render(<Button label="Click me" large />)
-    expect(getByText('Click me')).toHaveStyle({ height: 48 })
+    const { getByText, getByTestId } = render(<Button label="Click me" large />)
+    expect(getByTestId('button')).toHaveStyle({ height: 48 })
+    expect(getByText('Click me')).toHaveStyle({ fontSize: 20 })
+  })
+
+  it('renders outline button when outline is true', () => {
+    const { getByText, getByTestId } = render(<Button label="Click me" outline />)
+    expect(getByTestId('button')).toHaveStyle({ backgroundColor: 'transparent' })
+  })
+
+  it('renders custom style class name', () => {
+    const { getByTestId } = render(<Button label="Click me" styleClassName="bg-red-500" />)
+    expect(getByTestId('button')).toHaveStyle({ backgroundColor: '#ef4444' })
+  })
+
+  it('renders button with haptics when enableHaptics is true', () => {
+    const useHapticMock = jest.fn()
+
+    const { getByTestId } = render(<Button label="Click me" enableHaptics />)
+    fireEvent.press(getByTestId('button'))
+
+    // Asserting that useHaptic is called with 'light'
+    expect(useHaptic).toHaveBeenCalledWith('light')
+    // Asserting that the function returned by useHaptic is called
+    expect(useHapticMock).toHaveBeenCalled()
   })
 
   it('calls onPress function when button is pressed', () => {
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react'
 import { ActivityIndicator, Text, TouchableOpacity } from 'react-native'
-import { useHaptic } from '../../hooks/useHaptics'
+import { useHaptic } from '~/hooks/useHaptics'
 
 export interface ButtonProps {
   /** label of the button */
@@ -26,22 +26,17 @@ export const Button = forwardRef(function Button(
   { label, onPress, outline, styleClassName, large = true, enableHaptics, loading }: ButtonProps,
   ref: any,
 ) {
-  const hapticSuccess = useHaptic('success')
-  const hapticWarning = useHaptic('warning')
-
   return (
     <TouchableOpacity
       ref={ref}
       testID="button"
       onPress={() => {
-        if (enableHaptics) {
-          outline ? hapticWarning() : hapticSuccess()
-        }
+        if (enableHaptics) useHaptic('light')
         onPress && !loading && onPress()
       }}
-      className={`rounded-xl border-2 border-primary justify-center items-center ${
-        outline ? '' : 'bg-primary'
-      } ${large ? 'h-12 px-4 py-2' : 'px-3 py-1'} ${styleClassName} `}
+      className={`rounded-xl border-2 border-primary justify-center items-center bg-blue-400
+        ${outline ? 'bg-transparent' : 'bg-primary'}
+        ${large ? 'h-12 px-4 py-2' : 'px-3 py-1'} ${styleClassName}`}
     >
       <Text
         className={`text-base font-medium 
