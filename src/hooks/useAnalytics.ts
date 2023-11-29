c7840cfc62b56f313f650b8b359decb3f8b42e44
Milad <milaadd@gmail.com>
2023-11-29 01:34:31 +0000

    add useAnalytics hook

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -1,14 +1,18 @@
-// With hooks
 import { usePostHog } from 'posthog-react-native'
 
 export const useAnalytics = () => {
   const posthog = usePostHog()
 
   const capture = (event: string, properties?: Record<string, any>) => {
-    posthog.capture(event, properties)
+    posthog?.capture(event, properties)
+  }
+
+  const screen = (screenName: string) => {
+    posthog?.screen(screenName)
   }
 
   return {
     capture,
+    screen,
   }
 }
