231257edf8c1fead72a191fa0a1570226775b694
Milad <milaadd@gmail.com>
2023-12-18 17:24:11 +0000

    add safearea to ingedinet details

--- src/components/IngredientDetails.tsx
+++ src/components/IngredientDetails.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { View } from 'react-native'
 import { useFetchIngredientDetails, useSession } from '~/hooks'
 import { useAnalytics } from '~/hooks/useAnalytics'
+import { useSafeAreaInsetsStyle } from '~/utils/useSafeAreaInsetsStyle'
 import { Button } from './Button'
 import { Checkbox } from './Checkbox'
 import { HorizontalList } from './HorizontalList'
@@ -27,7 +28,7 @@ export const IngredientDetails = function IngredientDetails({
 }: IngredientDetailsProps) {
   const path = usePathname()
   const showCta = path !== '/add-ingredients'
-
+  const inset = useSafeAreaInsetsStyle(['bottom'], 'margin')
   const { screen, capture } = useAnalytics()
   const { isLoggedIn } = useSession()
   const {
@@ -49,7 +50,7 @@ export const IngredientDetails = function IngredientDetails({
   return (
     <>
       {loading || recipesLoading ? null : (
-        <View className="justify-start flex-1 p-6">
+        <View className="justify-start flex-1 px-6 pt-6 pb-6" style={inset}>
           {/* Name */}
           <View className="mb-3">
             {ingredient?.name && (
